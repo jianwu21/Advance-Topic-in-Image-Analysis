66cbefb882ea3ae35efe39b1f5a49f34abecd52d
Jian Wu <xcb479@alumni.ku.dk>
2017-12-28 15:10:59 +0100

    handle the test files

--- Object_Recognition/handle_orig_imges.py
+++ Object_Recognition/handle_orig_imges.py
@@ -3,9 +3,9 @@ import os
 
 from preprocess.process_leaf import process_scan_leaf
 
-def main():
+def process_train():
     leafscan_dict = pickle.load(open('./train.pickle', 'rb'))
-    all_classes = leafscan_dict.keys()
+    all_img_ids = leafscan_dict.keys()
 
     if os.path.exists('./process_train') \
         and len(os.listdir('./process_train')) == 12605:
@@ -13,10 +13,25 @@ def main():
         return
 
     # Example for showing the orignal pic
-    for family in all_classes[14:]:
-        for im_id in leafscan_dict.get(family):
-            ex_path = os.path.join('./train/', im_id + '.jpg')
-            process_scan_leaf(ex_path, output_folder='./process_train/')
+    for im_id in all_img_ids:
+        ex_path = os.path.join('./train/', im_id + '.jpg')
+        process_scan_leaf(ex_path, output_folder='./process_train/')
+
+
+def process_test():
+    leafscan_dict = pickle.load(open('./test.pickle', 'rb'))
+    all_img_ids = leafscan_dict.keys()
+
+    if os.path.exists('./process_test') \
+        and len(os.listdir('./process_test')) == 12605:
+        print('Handling has been done.')
+        return
+
+    # Example for showing the orignal pic
+    for im_id in all_img_ids:
+        ex_path = os.path.join('./test/', im_id + '.jpg')
+        process_scan_leaf(ex_path, output_folder='./process_test/')
 
 if __name__ == '__main__':
-    main()
+    process_train()
+    process_test()
