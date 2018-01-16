cbf292ab85c0c2ea80a63a020a96be4270b87191
Jian Wu <xcb479@alumni.ku.dk>
2018-01-16 23:12:17 +0100

    changes in demo

--- 3D Vision/demo.py
+++ 3D Vision/demo.py
@@ -2,14 +2,13 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-from features import find_correspondence_points
-
 from compute import *
+from features import find_correspondence_points
+from FmatrixModel import FundamentalMatrixModel
 
-im_1 = plt.imread('./rigidSfM/DSCN0956.JPG')
-im_2 = plt.imread('./rigidSfM/DSCN0953.JPG')
+im_1 = plt.imread('./rigidSfM/DSCN0976.JPG')
+im_2 = plt.imread('./rigidSfM/DSCN0972.JPG')
 
-plt.imshow(im_2)
 pts1, pts2 = find_correspondence_points(im_1, im_2)
 
 num = pts1.shape[1]
@@ -27,11 +26,24 @@ ax[1].imshow(cv2.cvtColor(im_2, cv2.COLOR_BGR2RGB))
 ax[1].plot(points2[0], points2[1], 'r.')
 
 # compute F
-F = compute_fundamental(points1, points2)
+model = FundamentalMatrixModel()
+F = model.fit(points1, points2)
+print(model.error(F, points1, points2))
 
 # compute the epipole
 e = compute_epipole(F)
 
-plot_epipolar_lines(im_1, im_2, points1, points2, F, show_epipole=True)
+# plot_epipolar_lines(im_1, im_2, points1, points2, F, show_epipole=True)
+plt.figure()
+plt.imshow(im_1)
+plt.plot(points1[0], points1[1], 'r.')
+for i in range(points1.shape[1]):
+    plot_epipolar_line(im_1, F, points2[:, i], epipole=None, show_epipole=True)
+
+plt.figure()
+plt.imshow(im_2)
+plt.plot(points2[0], points2[1], 'r.')
+for i in range(points1.shape[1]):
+    plot_epipolar_line(im_2, F.T, points1[:, i], epipole=None, show_epipole=True)
 
 plt.show()
